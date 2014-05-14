02a8199d412c613c40354d719e49bc16294563a2
Dan Nuffer <danielnuffer@gmail.com>
2014-05-14 13:45:57 -0600

    Implementing test_sections_plustache

--- src/test_sections_plustache.cpp
+++ src/test_sections_plustache.cpp
@@ -4,73 +4,27 @@
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
 
-#include "template.hpp"
-#include "plustache_types.hpp"
 #include "gmock_to_boost.hpp"
-
-// The fixture for testing class Foo.
-class SectionsTest : public ::testing::Test
-{
- public:
-    std::string result_string;
-    std::string result_file;
-    std::string template_string;
-    PlustacheTypes::ObjectType ctx;
-    std::string file;
-
-    SectionsTest()
-    {
-		 SetUp();
-    }
-
-    virtual ~SectionsTest()
-    {
-		 TearDown();
-    }
-
-    virtual void SetUp()
-    {
-        template_string = "Hi I am {{name}}.\n";
-        template_string += "{{# showme}}";
-        template_string += "I like {{pet}}.";
-        template_string += "{{/ showme}}";
-        template_string += "{{# dontshowme}}";
-        template_string += "If you see this, something went wrong.";
-        template_string += "{{/ dontshowme}}";
-        file = "sections.mustache";
-
-        std::ofstream myfile;
-        myfile.open (file.c_str());
-        myfile << template_string;
-        myfile.close();
-
-        ctx["name"] = "Daniel";
-        ctx["pet"] = "turtles";
-        ctx["showme"] = "true";
-        ctx["dontshowme"] = "false";
-        Plustache::template_t t;
-        result_string = t.render(template_string, ctx);
-        result_file = t.render(file, ctx);
-    }
-
-    virtual void TearDown()
-    {
-        remove(file.c_str());
-    }
-
-};
+#include "test_template_fixture.hpp"
 
 // Tests that a simple mustache tag is replaced
-TEST_F(SectionsTest, TestSectionsMustacheFromString)
+TEST_F(TemplateFixture, TestSectionsMustacheFromString)
 {
-    std::string expected = "Hi I am Daniel.\n";
-          expected += "I like turtles.";
-    EXPECT_EQ(expected, result_string);
+	template_string = "Hi I am {{name}}.\n";
+	template_string += "{{# showme}}";
+	template_string += "I like {{pet}}.";
+	template_string += "{{/ showme}}";
+	template_string += "{{# dontshowme}}";
+	template_string += "If you see this, something went wrong.";
+	template_string += "{{/ dontshowme}}";
+
+	set_tag_value("name", "Daniel");
+	set_tag_value("pet", "turtles");
+	set_tag_value("showme", "true");
+	set_tag_value("dontshowme", "false");
+
+	std::string expected = "Hi I am Daniel.\n";
+	expected += "I like turtles.";
+	EXPECT_EQ(expected, result);
 }
 
-TEST_F(SectionsTest, TestSectionsMustacheFromFile)
-{
-    std::string expected = "Hi I am Daniel.\n";
-          expected += "I like turtles.";
-    EXPECT_EQ(expected, result_file);
-}
