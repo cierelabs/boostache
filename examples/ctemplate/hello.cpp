533702b1dc1c5f4ee0a0e298939c84ac6129fc99
JeffGarland <jeff@crystalclearsoftware.com>
2014-05-14 05:33:33 -0600

    add in ctemplate hello examples

---
+++ examples/ctemplate/hello.cpp
@@ -0,0 +1,15 @@
+//user.cpp
+#include <cstdlib>
+#include <iostream>  
+#include <string>
+#include <ctemplate/template.h>  
+
+int main() {
+  std::string user = getenv("USER");
+  ctemplate::TemplateDictionary dict("example");
+  dict["NAME"] = user;
+  std::string output;
+  ctemplate::ExpandTemplate("example.tpl", ctemplate::DO_NOT_STRIP, &dict, &output);
+  std::cout << output;
+  return 0;
+}
