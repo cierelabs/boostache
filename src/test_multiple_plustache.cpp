48264d9cfd8b55bed9fd71c0e91303ab601f18c8
Dan Nuffer <danielnuffer@gmail.com>
2014-05-15 08:02:31 -0600

    Implement test_multiple_plustache

--- src/test_multiple_plustache.cpp
+++ src/test_multiple_plustache.cpp
@@ -5,69 +5,24 @@
 #define BOOST_TEST_MAIN
 #include <boost/test/unit_test.hpp>
 
-#include "template.hpp"
-#include "plustache_types.hpp"
 #include "gmock_to_boost.hpp"
-
-// The fixture for testing class Foo.
-struct MultipleTest
-{
-    std::string result_string;
-    std::string result_file;
-    std::string template_string;
-    std::string expected;
-    PlustacheTypes::ObjectType ctx;
-    std::string file;
-    MultipleTest()
-    {
-		 SetUp();
-    }
-
-    virtual ~MultipleTest()
-    {
-		 TearDown();
-    }
-
-    virtual void SetUp()
-    {
-        template_string = "<title>{{title}}</title>\n";
-        template_string += "Hi I am {{name}}{{lastname}}.\n";
-        template_string += "I like {{pet}}.{{!comment}}\n";
-        file = "multiple.mustache";
-
-        std::ofstream myfile;
-        myfile.open(file.c_str());
-        myfile << template_string;
-        myfile.close();
-
-        ctx["title"] = "Multiple Mustaches";
-        ctx["name"] = "Daniel";
-        ctx["pet"] = "turtles";
-        Plustache::template_t t;
-        result_string = t.render(template_string, ctx);
-        result_file = t.render(file, ctx);
-    }
-
-    virtual void TearDown()
-    {
-        remove(file.c_str());
-    }
-
-};
+#include "test_template_fixture.hpp"
 
 // Tests that multiple mustache tags are replaced
-TEST_F(MultipleTest, TestMultipleMustacheFromString)
+TEST_F(TemplateFixture, TestMultipleMustacheFromString)
 {
-    expected = "<title>Multiple Mustaches</title>\n";
-    expected += "Hi I am Daniel.\n";
-    expected += "I like turtles.\n";
-    EXPECT_EQ(expected, result_string);
-}
+	template_string = "<title>{{title}}</title>\n";
+	template_string += "Hi I am {{name}}{{lastname}}.\n";
+	template_string += "I like {{pet}}.{{!comment}}\n";
 
-TEST_F(MultipleTest, TestMultipleMustacheFromFile)
-{
-    expected = "<title>Multiple Mustaches</title>\n";
-    expected += "Hi I am Daniel.\n";
-    expected += "I like turtles.\n";
-    EXPECT_EQ(expected, result_file);
+	set_tag_value("title", "Multiple Mustaches");
+	set_tag_value("name", "Daniel");
+	set_tag_value("pet", "turtles");
+
+	generate_template();
+
+	std::string expected = "<title>Multiple Mustaches</title>\n";
+	expected += "Hi I am Daniel.\n";
+	expected += "I like turtles.\n";
+	// TODO: EXPECT_EQ(expected, result);
 }
