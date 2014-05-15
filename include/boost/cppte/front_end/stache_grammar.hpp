be5a54c46999bd31a85a5131e95fe0c5a3eb9e8c
Michael Caisse <mcaisse@ciere.com>
2014-05-14 21:26:45 -0700

    add some more initial files for collaboration

---
+++ include/boost/cppte/front_end/stache_grammar.hpp
@@ -0,0 +1,25 @@
+/**
+ *  \file stache_grammar.hpp
+ * 
+ *
+ *  Copyright 2014 Michael Caisse
+ *
+ *
+ *  Distributed under the Boost Software License, Version 1.0. (See accompanying
+ *  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+#ifndef BOOST_CPPTE_FRONT_END_STACHE_GRAMMAR_HPP
+#define BOOST_CPPTE_FRONT_END_STACHE_GRAMMAR_HPP
+
+#include <stache_ast.hpp>
+
+namespace boost { namespace cppte { namespace front_end
+{
+   struct stache_grammar
+   {
+      // rules
+   };
+}}}
+
+#endif
+
