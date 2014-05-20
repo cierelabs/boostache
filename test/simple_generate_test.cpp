38d5aef84ae6c85096888f9a07f67a1ecf8c401a
Michael Caisse <mcaisse@ciere.com>
2014-05-19 22:56:43 -0700

    add some comments in the example

--- test/simple_generate_test.cpp
+++ test/simple_generate_test.cpp
@@ -17,6 +17,15 @@
 
 namespace bstache = boost::boostache;
 
+// ----------------------------------------
+// The following specializations allow
+// the engine to understand how to handle
+// our model::stache_model type.
+//
+// Extending is simply a matter of supplying
+// the render and test methods that can be
+// found via ADL.
+// ----------------------------------------
 namespace boost { namespace boostache { namespace model
 {
    struct print_value
@@ -100,11 +109,15 @@ namespace boost { namespace boostache { namespace model
       return false;
    }
 }}}
+// ----------------------------------------
+// ----------------------------------------
 
 
 
 int main()
 {
+   // ------------------------------------------------------------------
+   // The input template
    std::string input( "Hello world \n"
                       "{{name}} is here.\n"
                       "{{& escaped_name}} is here\n"
@@ -115,20 +128,33 @@ int main()
                       "{{/bar}} done.\n"
                       "{{/foo}} done.\n"
       );
+   // ------------------------------------------------------------------
 
 
+   // ------------------------------------------------------------------
+   // The data model definition
    std::function<bool()> false_ = [](){return false;};
    std::function<bool()> true_  = [](){return true;};
 
    bstache::model::stache_model data = {{"name","Jeff"},{"whoot","yipee"},{"bar",false_},{"foo",true_}};
+   // ------------------------------------------------------------------
 
 
+   // ------------------------------------------------------------------
+   // load the template
+   // This parses the input and compiles the result. The return is the
+   // compiled data structure
    auto iter = input.begin();
    auto templ = bstache::load_template<bstache::format::stache>(iter,input.end());
+   // ------------------------------------------------------------------
 
-
+   
+   // ------------------------------------------------------------------
+   // Apply the compiled template and the data model to the generate
+   // method
    std::ostringstream stream;
    bstache::generate(stream, templ, data);
+   // ------------------------------------------------------------------
 
    std::cout << stream.str();
 
