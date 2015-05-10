a6a4c1c4c7f27d120c2fbdb32b6be14d3b0e40a2
Michael Caisse <mcaisse@ciere.com>
2015-05-10 11:49:27 -0700

    missed adding select_traits.hpp

---
+++ include/boost/boostache/model/select_traits.hpp
@@ -0,0 +1,54 @@
+/**
+ *  \file select_traits.hpp
+ *
+ *  Copyright 2014, 2015 Michael Caisse : ciere.com
+ *
+ *  Distributed under the Boost Software License, Version 1.0. (See accompanying
+ *  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+#ifndef BOOST_BOOSTACHE_MODEL_SELECT_TRAITS_HPP
+#define BOOST_BOOSTACHE_MODEL_SELECT_TRAITS_HPP
+
+#include <boost/optional.hpp>
+#include <boost/boostache/vm/traits.hpp>
+#include <boost/boostache/model/category.hpp>
+#include <type_traits>
+#include <map>
+#include <vector>
+
+
+namespace boost { namespace boostache { namespace extension
+{
+   // -------------------------------------------
+   // test category
+   template <typename T, typename Enable = void>
+   struct select_category
+      : mpl::identity<plain_attribute> {};
+
+   template <typename T>
+   struct select_category< T
+                         , typename enable_if<vm::trait::is_variant<T>>::type>
+      : mpl::identity<variant_attribute> {};
+
+   template <typename T>
+   struct select_category< T
+                         , typename std::enable_if<
+                              vm::trait::has_begin<
+                                 typename vm::trait::not_a_map<T>::type 
+                                 >::value 
+                              >::type
+                         >
+      : mpl::identity<container_attribute>
+   {};
+
+   template <typename T>
+   struct select_category<boost::optional<T>>
+      : mpl::identity<optional_attribute> {};
+
+   template <typename T>
+   struct select_category<std::map<std::string,T>>
+      : mpl::identity<associative_attribute> {};
+
+}}}
+
+#endif
