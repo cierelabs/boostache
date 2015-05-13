260b6c9aab538b7260261cd757460dffcca1190c
Michael Caisse <mcaisse@ciere.com>
2015-05-12 22:34:23 -0700

    Commit initial django parser testing infrastructure

---
+++ include/boost/boostache/frontend/django/printer.hpp
@@ -0,0 +1,80 @@
+/**
+ *  \file django/printer.hpp
+ *
+ *  Copyright 2015 Michael Caisse : ciere.com
+ *
+ *  Distributed under the Boost Software License, Version 1.0. (See accompanying
+ *  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+#ifndef BOOST_BOOSTACHE_FRONT_END_DJANGO_PRINTER_HPP
+#define BOOST_BOOSTACHE_FRONT_END_DJANGO_PRINTER_HPP
+
+#include <boost/variant/apply_visitor.hpp>
+#include <boost/boostache/frontend/django/ast.hpp>
+
+namespace boost { namespace boostache { namespace frontend { namespace django { namespace ast
+{
+   namespace detail
+   {
+      class printer
+      {
+      public:
+         typedef void result_type;
+
+         printer(std::ostream& out)
+            : out(out)
+         {}
+
+         template <typename T>
+         void operator()(T& v) const
+         {
+            out << "WHOA! We have an unimplemented type: " 
+                << typeid(v).name() << std::endl;
+         }
+
+         void operator()(undefined) const
+         {
+            out << "undefined" << std::endl;
+         }
+
+         void operator()(comment) const
+         {
+            out << "{# some comment #}";
+         }
+
+         void operator()(literal_text const & v) const
+         {
+            out << v;
+         }
+
+         void operator()(variable const & var) const
+         {
+            out << "{% ";
+            auto iter = var.begin();
+            auto iter_end = var.end();
+            while(iter != iter_end)
+            {
+               out << *iter;
+               if(++iter != iter_end)
+               {
+                  out << ".";
+               }
+            }
+         }
+
+      private:
+         std::ostream& out;
+      };
+   }
+
+   inline void print(std::ostream& out, node_list const& nodes)
+   {
+      detail::printer p(out);
+      for(auto const & node : nodes)
+      {
+         boost::apply_visitor(p, node);
+      }
+   }
+}}}}}
+
+#endif
---
+++ test/django/django_parser.cpp
@@ -0,0 +1,42 @@
+/**
+ *  \file test/django/django_parser.cpp
+ *
+ *  Link with shared/parser_test to test the django parser
+ *
+ *  Copyright 2015 Michael Caisse : ciere.com
+ *
+ *  Distributed under the Boost Software License, Version 1.0. (See accompanying
+ *  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#include <boost/test/unit_test.hpp>
+#include <boost/boostache/django.hpp>
+#include <boost/boostache/frontend/django/grammar_def.hpp>
+#include <boost/boostache/frontend/django/ast.hpp>
+#include <boost/boostache/frontend/django/printer.hpp>
+#include <boost/boostache/frontend/parse.hpp>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+namespace bstache = boost::boostache;
+namespace fe = boost::boostache::frontend;
+
+std::string print_ast(std::string const & filename)
+{
+   std::ifstream file(filename.c_str());
+   if(!file)
+   {
+      BOOST_CHECK_MESSAGE(false, "Failed to open " << filename);
+      return "";
+   }
+
+   std::ifstream istream(filename.c_str());
+   auto ast = fe::parse<bstache::format::django>(istream);
+   std::ostringstream stream;
+   fe::django::ast::print(stream,ast);
+   return stream.str();
+}
+
+
+std::string test_dir = "django/parser_test_dir";
