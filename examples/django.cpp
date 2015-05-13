1f9f6b32a53da4c5ac21ed3099a4da5c96c1da1c
Jeroen Habraken <vexocide@gmail.com>
2015-05-12 22:43:05 -0600

    Initial django parser

---
+++ examples/django.cpp
@@ -0,0 +1,58 @@
+/**
+ *  \file django.cpp
+ *
+ *  A simple example of how to use boostache.
+ *
+ *  Copyright 2015 Jeroen Habraken
+ *
+ *  Distributed under the Boost Software License, Version 1.0. (See accompanying
+ *  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+#define BOOST_SPIRIT_NO_PREDEFINED_TERMINALS
+
+#include <boost/boostache/boostache.hpp>
+#include <boost/boostache/django.hpp>
+#include <boost/boostache/frontend/django/grammar_def.hpp> // need to work out header only syntax
+#include <boost/boostache/model/helper.hpp>
+#include <iostream>
+#include <sstream>
+
+namespace boostache = boost::boostache;
+
+// Our input data type is going to be a map of strings to strings
+using map_t = std::map<std::string, std::string>;
+
+int main()
+{
+   // ------------------------------------------------------------------
+   // Describe the input template. We are going to use django format.
+   std::string input(
+         "My name is {{name}}. {# This is a comment #}I am {{age}} years old.\n");
+   // ------------------------------------------------------------------
+
+   // ------------------------------------------------------------------
+   // The data description. Just a simple map of strings to strings.
+   map_t data = { { "name"  , "Jeroen" },
+                  { "age"   , "42"     }
+   };
+   // ------------------------------------------------------------------
+
+   // ------------------------------------------------------------------
+   // Load the template.
+   // This parses the input and compiles the result. The return is the
+   // compiled data structure
+   using boostache::load_template;
+
+   auto iter = input.begin();
+   auto templ = load_template<boostache::format::django>(iter, input.end());
+   // ------------------------------------------------------------------
+
+   // ------------------------------------------------------------------
+   // Apply the compiled template and the data model to generate the result
+   std::stringstream stream;
+   boostache::generate(stream, templ, data);
+   // ------------------------------------------------------------------
+
+   // print the result
+   std::cout << stream.str();
+}
---
+++ include/boost/boostache/frontend/django/ast_adapted.hpp
@@ -0,0 +1,18 @@
+/**
+ *  \file ast_adapted.hpp
+ *
+ *  Fusion adaption for the stache ast
+ *
+ *  Copyright 2015 Jeroen Habraken
+ *
+ *  Distributed under the Boost Software License, Version 1.0. (See accompanying
+ *  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+#ifndef BOOST_BOOSTACHE_FRONT_END_DJANGO_AST_ADAPTED_HPP
+#define BOOST_BOOSTACHE_FRONT_END_DJANGO_AST_ADAPTED_HPP
+
+#include <boost/boostache/frontend/django/ast.hpp>
+#include <boost/fusion/include/adapt_struct.hpp>
+
+#endif
+
