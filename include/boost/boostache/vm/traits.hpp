ea79199d4257a5f98a36d8d7eb3c304ce6540546
Michael Caisse <mcaisse@ciere.com>
2016-09-24 15:48:43 -0700

    make traits more msvc friendly

--- include/boost/boostache/vm/traits.hpp
+++ include/boost/boostache/vm/traits.hpp
@@ -1,7 +1,7 @@
 /**
  *  \file traits.hpp
  *
- *  Copyright 2014, 2015 Michael Caisse : ciere.com
+ *  Copyright 2014 - 2016 Michael Caisse : ciere.com
  *
  *  Distributed under the Boost Software License, Version 1.0. (See accompanying
  *  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -9,7 +9,6 @@
 #ifndef BOOST_BOOSTACHE_VM_TRAITS_HPP
 #define BOOST_BOOSTACHE_VM_TRAITS_HPP
 
-#include <boost/mpl/bool.hpp>
 #include <boost/mpl/has_xxx.hpp>
 #include <boost/variant/variant.hpp>
 #include <map>
@@ -18,6 +17,9 @@
 
 namespace boost { namespace boostache { namespace vm { namespace trait
 {
+   // ----------------------------------------------------------------
+   // variant traits
+   // ----------------------------------------------------------------
    namespace detail
    {
       // this trait is enabled for the Spirit extended variant
@@ -31,12 +33,18 @@ namespace boost { namespace boostache { namespace vm { namespace trait
 
    template <typename T0, typename... TN>
    struct is_variant< boost::variant<T0,TN...> >
-      : mpl::true_
+      : std::true_type
    {};
 
    template <typename T>
    using enable_if_is_variant_t = typename std::enable_if<is_variant<T>::value>::type;
+   // ----------------------------------------------------------------
+   // ----------------------------------------------------------------
 
+
+   // ----------------------------------------------------------------
+   // begin traits
+   // ----------------------------------------------------------------
    using std::begin;
    template< class T
            , class = decltype(begin(std::declval<T>()))
@@ -45,21 +53,27 @@ namespace boost { namespace boostache { namespace vm { namespace trait
 
    std::false_type has_begin_test(...);
 
-   template<class T>
-   using has_begin = decltype(has_begin_test(std::declval<T>()));
+   template <typename T>
+   struct has_begin
+   {
+      using type = decltype(has_begin_test(std::declval<T>()));
+   };
 
    template<class T>
    using has_begin_t = typename has_begin<T>::type;
 
 
+   // ----------------------------------------------------------------
+   // map traits
+   // ----------------------------------------------------------------
    template<class T>
    struct is_map
-      : mpl::false_
+      : std::false_type
    {};
 
    template<class T1,class T2>
    struct is_map<std::map<T1,T2>>
-      : mpl::true_
+      : std::true_type
    {};
 
    template <typename T>
@@ -69,9 +83,11 @@ namespace boost { namespace boostache { namespace vm { namespace trait
    using enable_if_sequence_not_map_t =
       typename std::enable_if<
          std::integral_constant< bool
-                               , vm::trait::has_begin<T>::value
+                                 , vm::trait::has_begin_t<T>::value
                                  & !vm::trait::is_map<T>::value >::value
       >::type;
+   // ----------------------------------------------------------------
+   // ----------------------------------------------------------------
 
 }}}}
 
